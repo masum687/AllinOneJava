4df795b0ea72e1af26e560ec1e016aaef3f69c3b
Your Name <you@example.com>
2022-02-28 16:15:00 -0500

    Zeenat Apa bujtese

--- src/HashMap.java
+++ src/HashMap.java
@@ -3,7 +3,7 @@ public class HashMap {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Hello Zeenat Chy Apa");
+		System.out.println("Hello Zeenat Chy Apa What's up");
 
 	}
 
